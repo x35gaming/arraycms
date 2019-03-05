0f72e1d4a7a001a38a5d8e468e7af897a0e4e3e4
x35gaming <logan.howard.macfarl@gmail.com>
2019-03-05 20:08:03 +0800

    fixed up messy css

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
-<?php include('config.php') ?>
+<?php include('config.php');
+       ?>
 <html>
 	<head>
-		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<script src="https://cdn.jsdelivr.net/gh/leonardosnt/mc-player-counter/dist/mc-player-counter.min.js"></script>
 		<title><?php echo $sitename ?></title>
+		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<style>
-		#header {
-    		            background-color  : <?php echo $headcolor ?>;
-		            border            : 1px solid black  ;
-		    	    padding           : 5px              ;
+			#header {
+			    background-color  : <?php echo $headcolor ?>;
+		    	border            : 1px solid black  ;
+		    	padding           : 5px              ;
 			    margin            : 5px              ;
-	    		    text-align        : center           ;
+	    		text-align        : center           ;
 			}
 			
-		#navbar {
+			#navbar {
 			    background-color  : <?php echo $navcolor ?> ;
 			/*  height            : 200px            ;  */
 			    width             : 90%              ;
@@ -22,19 +23,19 @@
 			    border            : 1px solid black  ;
 			    padding           : 5px              ;
 			    margin            : auto             ;
-	    		    text-align        : left             ;
+	    		text-align        : left             ;
 			}
 			
-		#content {
-		        background-color  : <?php echo $contcolor ?>;
+			#content {
+				background-color  : <?php echo $contcolor ?>;
 		        height            : 400px            ; 
 		    	width             : 90%              ;  
-		        float             : center           ;
-			border            : 1px solid black  ;
-		        padding           : 5px              ;
-                        margin            : auto             ;
+				float             : center           ;
+				border            : 1px solid black  ;
+			    padding           : 5px              ;
+			    margin            : auto             ;
 	    		text-align        : left             ;
-                        overflow          : auto             ;
+                overflow          : auto             ;
 	    	}
 	    	
 	    	#footer {
@@ -70,8 +71,8 @@
         </div>
         
         </div>
-		<div id="content"><?php
-        include("cms/formatextensions.php")
+		<div id="content"><?php 
+		include("cms/formatextensions.php");
 function h($msg) {
 return "<h2>" . $msg . "</h2>" ;
 };
@@ -84,18 +85,17 @@ return "<a href=\"" . $link . "\" >" . $name . "</a>";
 function img($url) {
 return "<img src=\"" . $url . "\"/>" ;
 };
-require( "pages" . "/" . "n" . $_GET["page"] . ".php" ); 
+include( "pages" . "/" . "n" . $_GET["page"] . ".php" ); 
 
 foreach ($blog as $value) {
   echo "$value <br>";
 };
-echo "<p>this page was made with ❤ using <a href=\"http://x35gaminghub.rf.gd/?page=arraycms\"><b>arraycms</b></a>...</p>";
-include("cms/extensions.php")
+include ("cms/extensions.php");
+echo "<p>this page was made with ❤ using <a href=\"http://x35gaminghub.rf.gd/?page=arraycms\"><b>arraycms</b></a>...</p>"
 ?>  </div>
 		<div id="footer">
 		© <?php echo $owner ?>
 		<br><?php echo hyperlink($webmstremail, "mailto:" . $webmstremail ) ?>
-
 		</div>
 		
 	</body>
